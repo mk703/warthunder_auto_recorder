4a8d03a824a78dd8aee0ce84da4a9724493af478
mk703 <wh20160134@outlook.com>
2023-01-21 19:32:50 +0800

    mk703初次提交测试

---
+++ define_window_size.py
@@ -0,0 +1,15 @@
+import tomli
+import os
+
+
+def op():
+    f = open("config.toml", "rb")
+    toml_dict = tomli.load(f)
+    print(toml_dict)
+    h = toml_dict.get('window').get('height')
+    w = toml_dict.get('window').get('weight')
+    os.popen('mode con cols=' + h + 'lines=' + w)
+
+
+if __name__ == '__main__':
+    op()
